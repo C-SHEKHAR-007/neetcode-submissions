2154c39928ec8740c9b2b80f7b45c3c2b6811448
Chandra Shekhar Chaudhary <96561452+C-SHEKHAR-007@users.noreply.github.com>
2026-05-26 17:25:12 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        for (let p1 = 0; p1 < nums.length - 1; p1++) {
+            for (let p2 = p1 + 1; p2 < nums.length; p2++) {
+                if (nums[p1] + nums[p2] === target) return [p1, p2];
+            }
+        }
+    }
+}
