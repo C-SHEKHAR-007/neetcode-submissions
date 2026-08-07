b522b1b23618bac566e5c54289cca3b087fad18e
Chandra Shekhar Chaudhary <96561452+C-SHEKHAR-007@users.noreply.github.com>
2026-08-07 17:48:35 +0530

    Add: insertionSort - submission-0

---
+++ Data Structures & Algorithms/insertionSort/submission-0.js
@@ -0,0 +1,33 @@
+/**
+ * Pair class to store key-value pairs
+ */
+// class Pair {
+//     /**
+//      * @param {number} key The key to be stored in the pair
+//      * @param {string} value The value to be stored in the pair
+//      */
+//     constructor(key, value) {
+//         this.key = key;
+//         this.value = value;
+//     }
+// }
+class Solution {
+    /**
+     * @param {Pair[]} pairs
+     * @returns {Pair[][]}
+     */
+    insertionSort(pairs) {
+        let result = []
+        for(let i=0; i< pairs.length; i++) {
+            for(let j = i; j>0; j--){
+                if(pairs[j].key < pairs[j-1].key) {
+                    let temp = pairs[j];
+                    pairs[j] = pairs[j-1];
+                    pairs[j-1] = temp
+                }
+            }
+            result.push(JSON.parse(JSON.stringify(pairs)));
+        }
+        return result;
+    }
+}
